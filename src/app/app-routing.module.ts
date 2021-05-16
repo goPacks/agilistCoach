07b5f3e002627482d18b7ce5cbef411fd3b23906
hardeepsysbit <hardeep@sysbit.com>
2021-05-17 03:10:40 +0800

    202105170310

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TrainComponent } from './train/train.component';
+import { ShareComponent } from './share/share.component';
+import { ConsultComponent } from './consult/consult.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:  '', component:   HomeComponent , pathMatch:  'full' },
+  { path:  'share', component:  ShareComponent , pathMatch:  'full' },
+  { path:  'train', component:  TrainComponent  , pathMatch:  'full' },
+  { path:  'consult', component:  ConsultComponent  , pathMatch:  'full' },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+
+
+import { TrainComponent } from './train/train.component';
+import { ShareComponent } from './share/share.component';
+import { ConsultComponent } from './consult/consult.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainComponent,
+    ShareComponent,
+    ConsultComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    AppRoutingModule,
+    FlexLayoutModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
